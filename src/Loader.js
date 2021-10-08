6fe6672e3b3d273fafa740b32bcdcc56ce437759
Your Name <tanyakuzmenkotanya2@mail.ru>
2021-10-08 22:02:47 +0300

    ready cursReact

---
+++ src/Loader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const logger = () =>(
+    <div style={{display: 'flex', justifyContent: 'center', margin: '.5rem'}}>
+        <div className="lds-dual-ring"></div>
+    </div>
+)
+
+export default logger
